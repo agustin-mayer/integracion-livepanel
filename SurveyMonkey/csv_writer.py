1fd61d19f848b034ab8419c5fc2038e56d4b6f75
mayeragustin99 <mayeragustin99@gmail.com>
2024-05-22 00:11:07 -0300

    Revert "rename file and add process_csv method"
    This reverts commit 8f6de61dde7d5f5a4a7a1f51afd4e5d4f9ac0619.

---
+++ SurveyMonkey/csv_writer.py
@@ -0,0 +1,34 @@
+import csv
+
+class CSVWriter:
+    @staticmethod
+    def write_responses(filename, response_data, question_options):
+        with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["User Response ID"] + list(question_options.keys()))
+            for response in response_data["data"]:
+                response_row = [response["id"]] + ["0"] * len(question_options)
+                for page in response["pages"]:
+                    for question in page["questions"]:
+                        if "answers" in question:
+                            for answer in question["answers"]:
+                                    choice_id = answer.get("choice_id")
+                                    if choice_id in question_options:
+                                        column_index = list(question_options.keys()).index(choice_id)
+                                        response_row[column_index + 1] = "1"
+                                    else: #si el choice id no es una columna
+                                        column_index = list(question_options.keys()).index(question["id"])
+                                        text_response = ""
+                                        for answer in question["answers"]:
+                                            if "text" in answer:
+                                                text_response += answer.get("text") + " "
+                                        response_row[column_index + 1] = text_response.strip()
+                csv_writer.writerow(response_row)
+
+    @staticmethod
+    def write_options(filename, question_options):
+        with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+            csv_writer = csv.writer(csvfile)
+            csv_writer.writerow(["Choice ID", "Option"])
+            for choice_id, option in question_options.items():
+                csv_writer.writerow([choice_id, option])
