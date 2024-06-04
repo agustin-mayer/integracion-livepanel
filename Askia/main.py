41dc61646a4866b7d191437e95732806930ff4ce
mayeragustin99 <mayeragustin99@gmail.com>
2024-06-04 12:41:25 -0300

    rename folder xml responses

--- Askia/main.py
+++ Askia/main.py
@@ -6,7 +6,7 @@ from app.response_loader import load_responses_to_csv
 
 xml_file = './data/SurveyStructure.xml'
 csv_file = './data/output.csv'
-xml_responses_folder = './data/xml'
+xml_responses_folder = './data/xml_responses'
 
 # Generar los encabezados del CSV
 parse_xml_to_csv_headers_only(xml_file, csv_file)
