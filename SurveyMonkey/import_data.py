a8d7ef88b3543828a32ff76f46cb1a5a72e52c89
mayeragustin99 <mayeragustin99@gmail.com>
2024-06-10 11:30:46 -0300

    Revert "clean"
    This reverts commit d3cd4896bfaeb11cf0b53e7bbffdcad4e7aa54cf.

--- SurveyMonkey/import_data.py
+++ SurveyMonkey/import_data.py
@@ -1,5 +1,5 @@
 from csv_writer import write_responses
-from sMonkey_api_access import SurveyMonkeyAPI
+from sm_api_access import SurveyMonkeyAPI
 from dotenv import load_dotenv
 import os
 
@@ -8,13 +8,14 @@ def main():
     load_dotenv()
     sMonkey_token = os.getenv("SURVEYMONKEY_API_TOKEN")
     sMonkey_API_url = "api.surveymonkey.com"
-    sMonkey_svy_coll_id = "430914330"
-
     sMonkey_API = SurveyMonkeyAPI(sMonkey_token, sMonkey_API_url)
     
+    sMonkey_svy_coll_id = "430914330"
     responses = sMonkey_API.get_responses(sMonkey_svy_coll_id)
     
+    # Escribir las respuestas en un archivo CSV directamente desde los datos JSON
     write_responses(f"{sMonkey_svy_coll_id}_responses.csv", responses)
+    print("Las respuestas se guardaron con éxito.")
 
     """
     livepanel_token = os.getenv("LIVEPANEL_API_TOKEN")
