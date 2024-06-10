5a75b4525b381b5885043cce9f624ee1144e75e2
mayeragustin99 <mayeragustin99@gmail.com>
2024-06-10 02:19:49 -0300

    refactor

--- SurveyMonkey/import_data.py
+++ SurveyMonkey/import_data.py
@@ -1,7 +1,5 @@
+from csv_writer import write_responses
 from sm_api_access import SurveyMonkeyAPI
-from option_processor import process_options
-from json_handler import JSONHandler
-from csv_handler import CSVHandler
 from dotenv import load_dotenv
 import os
 
@@ -10,20 +8,14 @@ def main():
     load_dotenv()
     sMonkey_token = os.getenv("SURVEYMONKEY_API_TOKEN")
     sMonkey_API_url = "api.surveymonkey.com"
-    sMonkey_API = SurveyMonkeyAPI(sMonkey_token,sMonkey_API_url)
+    sMonkey_API = SurveyMonkeyAPI(sMonkey_token, sMonkey_API_url)
     
     sMonkey_svy_coll_id = "430914330"
     responses = sMonkey_API.get_responses(sMonkey_svy_coll_id)
     
-    sMonkey_svy_id = "412807489"
-    survey_data = sMonkey_API.get_options(sMonkey_svy_id)
-    CSVHandler.write_options(f"./sm_data/{sMonkey_svy_coll_id}_options.csv", survey_data)
-    
-    options = process_options(survey_data)
-    CSVHandler.write_responses(f"./sm_data/{sMonkey_svy_coll_id}_responses.csv", responses, options)
-    JSONHandler.write_responses(f"./sm_data/{sMonkey_svy_coll_id}_responses.json", responses)
-    
-    print("Archivos CSV y JSON generados exitosamente.")
+    # Escribir las respuestas en un archivo CSV directamente desde los datos JSON
+    write_responses(f"{sMonkey_svy_coll_id}_responses.csv", responses)
+    print("Las respuestas se guardaron con éxito.")
 
     """
     livepanel_token = os.getenv("LIVEPANEL_API_TOKEN")
