1854a0a41e5e21cfeaaebcd6c9c8307b0e7547fe
mayeragustin99 <mayeragustin99@gmail.com>
2024-06-10 01:57:07 -0300

    new json to csv P.Q_O

---
+++ SurveyMonkey/json_to_csv.py
@@ -0,0 +1,45 @@
+import csv 
+
+def write_responses(filename, response_data):
+    # Crear un conjunto para almacenar todas las claves de respuesta únicas
+    response_keys = set()
+
+    # Recorrer las respuestas y almacenar las claves de respuesta únicas
+    for response in response_data["data"]:
+        for page in response["pages"]:
+            for question in page["questions"]:
+                question_id = question["id"]
+                page_id = page["id"]
+                question_key = f"P{page_id}.Q{question_id}"
+                if "answers" in question:
+                    for answer in question["answers"]:
+                        choice_id = answer.get("choice_id")
+                        if choice_id:
+                            response_keys.add(f"{question_key}_{choice_id}")
+                        elif "text" in answer:
+                            response_keys.add(f"{question_key}")
+
+    # Crear la lista de encabezados con los IDs de las preguntas
+    headers = ["User Response ID"] + list(response_keys)
+
+    with open(filename, "w", newline="", encoding="utf-8") as csvfile:
+        csv_writer = csv.writer(csvfile)
+        csv_writer.writerow(headers)
+        
+        for response in response_data["data"]:
+            response_row = [response["id"]]
+            response_values = {key: "0" for key in response_keys}  # Inicializar todas las respuestas como "0"
+            for page in response["pages"]:
+                for question in page["questions"]:
+                    question_id = question["id"]
+                    page_id = page["id"]
+                    question_key = f"P{page_id}.Q{question_id}"
+                    if "answers" in question:
+                        for answer in question["answers"]:
+                            choice_id = answer.get("choice_id")
+                            if choice_id:
+                                response_values[f"{question_key}_{choice_id}"] = "1"
+                            elif "text" in answer:
+                                response_values[f"{question_key}"] = answer["text"]
+            response_row.extend(response_values.values())
+            csv_writer.writerow(response_row)
